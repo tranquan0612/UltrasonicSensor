876f4e6f1d53dea4f30e76010133db8ea1f24bb7
Trần Hoàng Quân <79348852+tranquan0612@users.noreply.github.com>
2025-08-10 10:43:53 +0700

    Update basic_usage.ino

--- examples/basic_usage/basic_usage.ino
+++ examples/basic_usage/basic_usage.ino
@@ -1,1 +1,17 @@
+#include "UltrasonicSensor.h"
+
+// Tạo đối tượng cảm biến
+MyUltrasonic sensor(5, 18);
+
+void setup() {
+    Serial.begin(115200);
+}
+
+void loop() {
+    long distance = sensor.getDistance();
+    Serial.print("Khoang cach: ");
+    Serial.print(distance);
+    Serial.println(" cm");
+    delay(500);
+}
 
