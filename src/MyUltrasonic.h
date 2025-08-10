79b238b7cfe3355bc2b8c25ffa59b6a86866e9e1
Trần Hoàng Quân <79348852+tranquan0612@users.noreply.github.com>
2025-08-10 09:16:18 +0700

    Update MyUltrasonic.h

--- src/MyUltrasonic.h
+++ src/MyUltrasonic.h
@@ -1,1 +1,16 @@
+#ifndef MYULTRASONIC_H
+#define MYULTRASONIC_H
+
+#include <Arduino.h>
+
+class MyUltrasonic {
+private:
+    int trigPin;
+    int echoPin;
+public:
+    MyUltrasonic(int trig, int echo);
+    long getDistance();
+};
+
+#endif
 
